a985db58cd924a050642ea470278d351ef666614
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-26 14:19:37 +0300

    squash!

--- Source/extension.ts
+++ Source/extension.ts
@@ -4,53 +4,52 @@ import path = require("path");
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "vicperdana.psdocs-vscode-preview",
-    function () {
-      var currentlyOpenFilePath =
-        vscode.window.activeTextEditor?.document.uri.fsPath;
+	const disposable = vscode.commands.registerCommand(
+		"vicperdana.psdocs-vscode-preview",
+		() => {
+			var currentlyOpenFilePath =
+				vscode.window.activeTextEditor?.document.uri.fsPath;
 
-      var templatePath = "";
-      var outputPath = "";
-      var templateFolderPath = "";
-      let options: vscode.InputBoxOptions = {
-        prompt: "Full path to the ARM template: ",
-        value: `${currentlyOpenFilePath}`,
-      };
+			var templatePath = "";
+			var outputPath = "";
+			var templateFolderPath = "";
+			const options: vscode.InputBoxOptions = {
+				prompt: "Full path to the ARM template: ",
+				value: `${currentlyOpenFilePath}`,
+			};
 
-      vscode.window.showInputBox(options).then((value) => {
-        if (!value) return;
-        templatePath = value;
-        templateFolderPath = path.dirname(templatePath);
-        let options: vscode.InputBoxOptions = {
-          prompt:
-            "Output Path of the Markdown (relative path from the ARM template file): ",
-          value: "out\\docs\\",
-        };
-        vscode.window.showInputBox(options).then((value) => {
-          if (!value) return;
-          outputPath = value;
+			vscode.window.showInputBox(options).then((value) => {
+				if (!value) return;
+				templatePath = value;
+				templateFolderPath = path.dirname(templatePath);
+				const options: vscode.InputBoxOptions = {
+					prompt: "Output Path of the Markdown (relative path from the ARM template file): ",
+					value: "out\\docs\\",
+				};
+				vscode.window.showInputBox(options).then((value) => {
+					if (!value) return;
+					outputPath = value;
 
-          const { exec } = require("child_process");
-          var message = "";
-          exec(
-            `Import-Module PSDocs.Azure; Invoke-PSDocument -Module PSDocs.Azure -InputObject ${templatePath} -OutputPath ${templateFolderPath}/${outputPath};`,
-            { shell: "pwsh" },
-            (error: any, stdout: any, stderr: any) => {
-              message = stderr;
-              if (message !== "") {
-                vscode.window.showErrorMessage(message);
-              } else {
-                vscode.window.showInformationMessage(
-                  `Markdown generated in ${templateFolderPath}/${outputPath}`
-                );
-              }
-            }
-          );
-        });
-      });
-    }
-  );
+					const { exec } = require("child_process");
+					var message = "";
+					exec(
+						`Import-Module PSDocs.Azure; Invoke-PSDocument -Module PSDocs.Azure -InputObject ${templatePath} -OutputPath ${templateFolderPath}/${outputPath};`,
+						{ shell: "pwsh" },
+						(error: any, stdout: any, stderr: any) => {
+							message = stderr;
+							if (message !== "") {
+								vscode.window.showErrorMessage(message);
+							} else {
+								vscode.window.showInformationMessage(
+									`Markdown generated in ${templateFolderPath}/${outputPath}`,
+								);
+							}
+						},
+					);
+				});
+			});
+		},
+	);
 }
 
 export function deactivate() {}
